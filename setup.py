66dc59fed3cf6c8491174591a913715589c9c888
Thibaut Horel <thibaut.horel@gmail.com>
2014-08-14 14:06:22 -0400

    Update setup.py, add CHANGELOG

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup
 
 setup(name='tabletext',
       version='0.1',
-      description='Pretty-print tabular data',
-      long_description=open("README.rst").read(),
-      url='https://github.com/Thibauth/python-pushover',
+      description='Python library and command line utility to pretty-print\
+      tabular data',
+      long_description=open("README.rst").read() + "\n"
+      + open("CHANGELOG.rst").read(),
+      url='https://github.com/Thibauth/tabletext',
       author='Thibaut Horel',
-      author_email='thibaut.hore+tabletext@gmail.com',
+      author_email='thibaut.horel+tabletext@gmail.com',
       py_modules=['tabletext'],
       entry_points={"console_scripts": ["table=tabletext:main"]},
       use_2to3=True,
