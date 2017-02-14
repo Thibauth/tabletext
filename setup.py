18b47d196ad851a3ab5fd040484a3ed628134a1c
John W. O'Brien <john@saltant.com>
2017-02-14 18:28:51 -0500

    BUG: Set explicit encoding for py3k+

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+from io import open
+
 from setuptools import setup
 
 setup(name='tabletext',
       version='0.1',
       description='Python library and command line utility to pretty-print\
       tabular data',
-      long_description=open("README.rst").read() + "\n"
-      + open("CHANGELOG.rst").read(),
+      long_description=open("README.rst", encoding='utf-8').read() + "\n"
+      + open("CHANGELOG.rst", encoding='utf-8').read(),
       url='https://github.com/Thibauth/tabletext',
       author='Thibaut Horel',
       author_email='thibaut.horel+tabletext@gmail.com',
